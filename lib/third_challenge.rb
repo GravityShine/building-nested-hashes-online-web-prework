40f55859acbb03e4776c2ec3aae3092bd84fd366
Samuel Kovacs <samuelbkovacs@gmail.com>
2019-04-01 09:35:43 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,19 +1,9 @@
 def third_challenge
   epic_tragedy = {
-   :montague => {
-      :patriarch => {
-      :name => "Lord Montague"
-      :age => "53"
- },
+   :montague => {:patriarch => {:name => "Lord Montague", :age => "53"},
         },
-      :matriarch => {
-        :name => "Lady Montague",
-      :age => "54"
-        },
-      :hero => {
-        :name => "Romeo",
-     :age => "15"
-        },
+      :matriarch => {:name => "Lady Montague",:age => "54"},
+      :hero => {:name => "Romeo",:age => "15"},
       :hero_friends => [ {
          :name => "Benvolio",
          :age => "17",
